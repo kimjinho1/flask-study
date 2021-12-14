3d8db1cd233e8a6383f1ec98e64ec429ddb752f6
kimjinho1 <rlawlsgh8113@naver.com>
2021-12-14 23:18:56 +0900

    2021-12-14

---
+++ REST_API_User/models.py
@@ -0,0 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    password = db.Column(db.String(2))
+    userid = db.Column(db.String(4))
+    username = db.Column(db.String(8))
